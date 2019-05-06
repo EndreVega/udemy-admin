54517d3be853952d33a9fa3ecddd4a73e568d80a
Endre Huaman de la Vega <endre_15@icloud.com>
2019-05-06 12:26:01 -0500

    Input, output y viewdchild

---
+++ src/app/components/grafico-dona/grafico-dona.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Label, MultiDataSet } from 'ng2-charts';
+import { ChartType } from 'chart.js';
+
+@Component({
+  selector: 'app-grafico-dona',
+  templateUrl: './grafico-dona.component.html'
+})
+export class GraficoDonaComponent implements OnInit {
+
+  @Input('chartLabels') doughnutChartLabels: Label[] = [];
+  @Input('chartData') doughnutChartData: MultiDataSet = [];
+  @Input('chartType') doughnutChartType: string = '';
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from "@angular/core";
 import { PAGES_ROUTES } from './pages.routes';
+import { FormsModule } from "@angular/forms";
+
+//ng2- charts
+import { ChartsModule } from 'ng2-charts';
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -8,12 +12,18 @@ import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 
+//temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
 @NgModule({
     declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     exports: [
         DashboardComponent,
@@ -22,7 +32,9 @@ import { PagesComponent } from './pages.component';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ]
 })
 export class PagesModule { }
